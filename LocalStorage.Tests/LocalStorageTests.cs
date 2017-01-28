fe5c496ead9c9c155c79fb7d0df95072eaf9bbe5
hanssens <github@hanssens.com>
2017-01-28 17:20:29 +0100

    Added clean, empty project structure

---
+++ LocalStorage.Tests/LocalStorageTests.cs
@@ -0,0 +1,10 @@
+using System;
+using Xunit;
+
+namespace LocalStorage.Tests
+{
+    public class LocalStorageTests
+    {
+
+    }
+}
---
+++ LocalStorage/LocalStorage.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace LocalStorage
+{
+    public class LocalStorage
+    {
+    }
+}
