ec2bca89be9076c172f74ea15a6fd2a530ff33b3
hanssens <github@hanssens.com>
2017-02-10 20:55:29 +0100

    Added stub models Car + CarFactory

---
+++ LocalStorage.Tests/Stubs/Car.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LocalStorage.Tests.Stubs
+{
+    public class Car
+    {
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+    }
+}
