01ca487bb0e0f4d091be1e3a816c8b5e3169a786
hanssens <github@hanssens.com>
2017-02-12 13:42:32 +0100

    Added signature only for encrypt/decrypt helpers

---
+++ LocalStorage.Tests/LocalStorageTests.Encryption.cs
@@ -0,0 +1,26 @@
+﻿using FluentAssertions;
+using Hanssens.Net;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using Xunit;
+using LocalStorageTests.Stubs;
+
+namespace LocalStorageTests
+{
+    public partial class EncryptionTests
+    {
+        [Fact(DisplayName = "Helpers.Decrypt() should decode an encrypted string", Skip = "TODO")]
+        public void Decrypt_Should_Decode_An_Encrypted_String()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact(DisplayName = "Helpers.Encrypt() should encrypt a string", Skip = "TODO")]
+        public void Encryption_Should_Encrypt_String()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- LocalStorage/Helpers.cs
+++ LocalStorage/Helpers.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 
 namespace Hanssens.Net
 {
@@ -7,6 +8,16 @@ namespace Hanssens.Net
         internal static string GetLocalStoreFilePath(string filename)
         {
             return Path.Combine(System.AppContext.BaseDirectory, filename);
+        }
+
+        internal static string Decrypt(string encryptionKey, string encryptedValue)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static string Encrypt(string encryptionKey, string rawValue)
+        {
+            throw new NotImplementedException();
         }
     }
 }
